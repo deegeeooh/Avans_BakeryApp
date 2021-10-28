f4f22895cf6d150e4fd75aca801b84da1b1f52e4
deegeeooh <bakkerpepijn69@gmail.com>
2021-10-28 11:20:22 +0200

    added mutation class for data mutations

---
+++ Mutation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vlaaieboer
+{
+    class Mutation
+    {
+        public int RecordCounter { get; set; }
+        public DateTime MutationDate { get; set; }
+        public string oldValue { get; set;} 
+        public string newValue { get; set; }
+    }
+}
