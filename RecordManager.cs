08cbf987870b0f2ef223f4967cb6cd30c1ac85f2
deegeeooh <bakkerpepijn69@gmail.com>
2021-11-25 13:07:19 +0100

    Refactored Recordmanager: added ID and SearchString

--- RecordManager.cs
+++ RecordManager.cs
@@ -4,25 +4,79 @@ using System.Collections.Generic;
 
 namespace BakeryConsole
 {
-    class RecordManager
+    class RecordManager 
     {
-        public int RecordCounter                       { get; set; }    // NOT static, this is a record propery of the  class    
-        public static int TotalRecords                 { get; set; }    // static,this a class property        
-        public bool Active                             { get; set; }    // flag for record deletion/inactive
-        public List<Mutation> Mutations                { get; set; }    // just as PoC; every record stores all mutations 
-                                                                        // in practice, store in separate file.
-        public RecordManager()
+
+        private static int lengthQuestionField      = 30;
+        private static string checkinputStringAlpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/-@|' .,_";
+        
+        private static int[,] fieldProperties       = { { 0,   8,  1 },             //NICE : Use attributes instead
+                                                        { 1,  45,  1 } };
+
+        public static String[] fieldNames           = { "Record ID:",               // 0
+                                                        "Name:"        };           // 1
+
+        public string ID                                { get; set; }    // unique record ID
+        public string SearchString                      { get; set; }    // Default record search string
+        public int RecordCounter                        { get; set; }    // NOT static, this is a record propery of the  class    
+        public static int TotalRecords                  { get; set; }    // static,this a class property        
+        public bool Active                              { get; set; }    // flag for record deletion/inactive
+        public List<Mutation> Mutations                 { get; set; }    // just as PoC; every record stores all mutations 
+
+        public RecordManager(string aStringFor_Name)
         {
+            fieldNames[1]   = aStringFor_Name;
             TotalRecords++;
             RecordCounter   = TotalRecords;
+            SearchString    = IO.GetInput(fieldNames[1], "", checkinputStringAlpha, lengthQuestionField, fieldProperties[1, 1], false, true, true, true, true, fieldProperties[1, 2]);
+            ID              = ConstructID(this);
             Active          = true;
-
             CheckMutations(this, " ", "[Created:]", "", 0);             // set creation date in mutation at initial record creation
         }
 
-        public RecordManager(bool dontDoShit) { }                       // DUMMY
+        public RecordManager(bool _clearForm, string aStringFor_Name, bool _First) 
+        {
+            if (_First)
+            {
+                fieldNames[1] = aStringFor_Name;
+                var cursor = Console.CursorTop;
+                    for (int i = 0; i < fieldProperties.GetLength(0); i++)
+                    {
+                        IO.PrintBoundaries(fieldNames[i], "", lengthQuestionField, fieldProperties[i, 1], cursor, false); Console.WriteLine(); cursor++;
+                    }
+            }
+        }                       
 
-        public RecordManager(RecordManager anInheritor,bool dontDoShit) { }     
+        public RecordManager(RecordManager anInheritor, bool displayOnly, string aStringFor_Name, bool _First ) 
+        {
+            if (_First)
+            {
+                if (!displayOnly)           //EDIT
+                {
+                    RecordCounter = anInheritor.RecordCounter;
+                    SearchString = IO.GetInput(fieldNames[1], anInheritor.SearchString, checkinputStringAlpha, lengthQuestionField, fieldProperties[1, 1], false, true, true, true, true, fieldProperties[1, 2]);
+                    ID = ConstructID(this);
+                    this.Mutations = anInheritor.Mutations;
+                    Active = true;
+
+                    CheckMutations(anInheritor, anInheritor.ID, this.ID, fieldNames[0], anInheritor.Mutations.Count);
+                    CheckMutations(anInheritor, anInheritor.SearchString, this.SearchString, fieldNames[1], anInheritor.Mutations.Count);
+                }
+                else               // DISPLAY ONLY
+                {
+                    int cursorColumn = Console.CursorTop;
+                    IfActive(anInheritor, lengthQuestionField + fieldProperties[0, 1] + 5, cursorColumn);
+                    IO.PrintBoundaries(fieldNames[0], anInheritor.ID, lengthQuestionField, fieldProperties[0, 1], cursorColumn, anInheritor.Active); Console.WriteLine(); cursorColumn++;
+                    IO.PrintBoundaries(fieldNames[1], anInheritor.SearchString, lengthQuestionField, fieldProperties[1, 1], cursorColumn, anInheritor.Active); Console.WriteLine(); cursorColumn++;
+
+                }
+            }
+        }
+
+
+        public RecordManager( bool dontDoShit ) { }                      // dummy 
+
+        public RecordManager(RecordManager anInheritor, bool dontDoShit) { }     
 
         [JsonConstructor]                                               // for json, otherwise it will use the default() constructor when deserializing which we don't want here
         public RecordManager(Int64 JUST4JSON_DontCall)
@@ -44,7 +98,7 @@ namespace BakeryConsole
                                            fieldName,
                                            old,
                                            "",                            // placeholder because:
-                                                                          //newVal.Replace(old, ""),     // TODO: old cannot be empty, throws exception
+                                                                          //newVal.Replace(old, "") // TODO: old cannot be empty, throws exception
                                            newVal);
                 
                 anInheritor.Mutations.Add(newMutation);                       // needs object reference when = null;
@@ -64,7 +118,6 @@ namespace BakeryConsole
             {
                 IO.SystemMessage("Record has been marked for Deletion, changes written to file", false);
             }
-
         }
 
         public static void SetTotalRecords(int aRecord)
@@ -77,7 +130,12 @@ namespace BakeryConsole
             TotalRecords = 0;
         }
 
-        public static void IfActive<T>(T anInheritor,  int onCursorColumn , int onCursorRow ) where T : RecordManager
+        public static string RetrieveSearchString<T>( T anInheritor, int aRecordNumber ) where T : RecordManager
+        {
+            string searchString = anInheritor.SearchString;
+            return searchString;
+        }
+        public void IfActive<T>(T anInheritor,  int onCursorColumn , int onCursorRow ) where T : RecordManager
         {
             if (!anInheritor.Active)
             {
@@ -88,6 +146,24 @@ namespace BakeryConsole
                 IO.PrintOnConsole("".PadRight(12, ' '), onCursorColumn, onCursorRow, Color.TextColors.Defaults);
             }
         }
+
+        private string ConstructID(RecordManager aRecord)
+        {
+            string a = RecordCounter.ToString("D5");            // make a string consisting of 5 decimals
+            string b;
+            if (aRecord.SearchString.Length >= 3)
+            {
+                b = aRecord.SearchString.Substring(0, 3).ToUpper();             // take first 3 chars in uppercase
+            }                                                             // TODO: remove whitespace if exists ("de Groot")
+            else
+            {
+                b = aRecord.SearchString.Substring(0, aRecord.SearchString.Length)    // or build to 3 chars with added "A" chars
+                    .ToUpper()
+                    .PadRight(3, 'A');
+            }
+            return b + a;
+        }
+
 
     }
 }
