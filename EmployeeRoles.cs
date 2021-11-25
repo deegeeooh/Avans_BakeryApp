241d81e3a01da9b6224a940b8b1bb6478c551efd
deegeeooh <bakkerpepijn69@gmail.com>
2021-11-25 13:15:38 +0100

    Added EmployeeRoles

--- EmployeeRoles.cs
+++ EmployeeRoles.cs
@@ -1,17 +1,58 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+
 
 namespace BakeryConsole
 {
-    internal class EmployeeRoles
+    internal class EmployeeRoles : RecordManager
     { 
-        private static int[,] fieldProperties = { { 0,  3, 1 },
-                                                  { 1, 45, 1 } };
+        public static int lengthQuestionField = 30;
 
-        private static String[] fieldNames = { "Code: ",                                      //0
-                                               "Description: " };
+        // input validation string
+        public static string checkinputStringAlpha  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789//-@| '.,_";
+        private static string _SearchStringName     = "Description";
 
-        public int RecordCounter { get; set; }
+        private static int[,] fieldProperties       = { { 0, 3, 1 } };
+
+
+        private static String[] fieldNames          = { "Code: " };                                      //0
+                                               
+
         public string Code { get; set; }
-        public string Description { get; set; }
+        // public string Description { get; set; }
+
+        public EmployeeRoles() : base(_SearchStringName)
+        {
+            Code             = IO.GetInput(fieldNames[0],  "", checkinputStringAlpha, lengthQuestionField, fieldProperties[0, 1], true, true, true, true, true, fieldProperties[0, 2]);
+        }
+        public EmployeeRoles(bool clearForm, bool _Activatordummy) : base (clearForm, _SearchStringName,  true)                                   // Constructor for displaying clear input form
+        {
+            var cursor = Console.CursorTop;
+            for (int i = 0; i < fieldProperties.GetLength(0); i++)
+            {
+                IO.PrintBoundaries(fieldNames[i], "", lengthQuestionField, fieldProperties[i, 1], cursor, false); Console.WriteLine(); cursor++;
+            }
+        }
+
+        public EmployeeRoles( EmployeeRoles anEmployeeRole, bool displayOnly ) : base(anEmployeeRole, displayOnly, _SearchStringName, true)
+        {
+            if (!displayOnly)            //Edit
+            {
+                 Code             = IO.GetInput(fieldNames[0],  anEmployeeRole.Code, checkinputStringAlpha, lengthQuestionField, fieldProperties[0, 1], true, true, true, true, true, fieldProperties[0, 2]);
+                 CheckMutations(anEmployeeRole, anEmployeeRole.Code, this.Code, fieldNames[0], anEmployeeRole.Mutations.Count);
+            }
+            else                        // Display Only
+            {
+                int cursorColumn = Console.CursorTop;
+                IO.PrintBoundaries(fieldNames[0], anEmployeeRole.Code, lengthQuestionField, fieldProperties[0, 1], cursorColumn, anEmployeeRole.Active); Console.WriteLine(); cursorColumn++;
+            }
+        }
+
+        [JsonConstructor]                                              
+        public EmployeeRoles(Int64 JUST4JSON_DontCall) : base (JUST4JSON_DontCall)
+        {
+            //Console.WriteLine("Don't be a dick Jason dear"); Console.ReadKey();
+        }
+
     }
 }
