bce66ea3c9134f0c2509005f0af677cda378e18a
deegeeooh <bakkerpepijn69@gmail.com>
2021-12-04 19:12:30 +0100

    Table and Window classes to build an display boxes and tables

---
+++ Table.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakeryConsole
+{
+    internal class Table
+
+    {
+        public List<Window> Columns { get; set; }
+
+        public Table(List<Window> columns)
+        {
+            Columns = columns;
+        }
+    }
+}
