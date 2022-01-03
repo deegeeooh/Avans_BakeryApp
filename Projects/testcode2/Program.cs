60bd9961da345b754128741acba6c682292b44c0
deegeeooh <bakkerpepijn69@gmail.com>
2022-01-03 12:36:47 +0100

    teststuff

--- Projects/testcode2/Program.cs
+++ Projects/testcode2/Program.cs
@@ -7,7 +7,16 @@ class Program
 
         static void Main(string[] args)
         {
+            string searchString = "12";
+            string toBeSearched = "Hoogstraat 12";
             
+            if (toBeSearched.Contains(searchString))
+            {
+                Console.WriteLine("\"{0}\" contains \"{1}\"",toBeSearched,searchString);
+            }else
+            {
+                Console.WriteLine("\"{0}\" does not contain \"{1}\"",toBeSearched,searchString);
+            }
         }
 
     }
